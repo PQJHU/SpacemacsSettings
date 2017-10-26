9853db7adb230e85be173a37e11865c56a14a1e4
Junjie Hu <jeremy.junjie.hu@gmail.com>
2017-10-26 10:03:27 +0200

    no good to use, ready to go back emacs

--- init.el
+++ init.el
@@ -325,6 +325,10 @@ you should place your code here."
   (setq powerline-default-separator 'arrow)
   (define-key evil-insert-state-map (kbd "C-e") 'mwim-end-of-code-or-line)
   (define-key evil-motion-state-map (kbd "C-e") 'mwim-end-of-code-or-line)
+  (with-eval-after-load 'python
+    (remove-hook 'python-mode-hook #'python-setup-shell))
+  (setq python-shell-interpreter "ipython3")
+  (setq org-latex-listings 'minted)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
@@ -339,6 +343,7 @@ you should place your code here."
  '(exec-path-from-shell-arguments (quote ("-l")))
  '(hlt-auto-face-foreground nil)
  '(org-export-backends (quote (ascii beamer html icalendar latex md odt)))
+ '(org-latex-listings (quote minted))
  '(package-selected-packages
    (quote
     (multiple-cursors web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode haml-mode emmet-mode company-web web-completion-data lispy zoutline ess-smart-equals ess-R-data-view ctable ess julia-mode company-auctex auctex yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode dash-functional cython-mode company-anaconda anaconda-mode pythonic reveal-in-osx-finder pbcopy osx-trash osx-dictionary launchctl org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download htmlize gnuplot youdao-dictionary names chinese-word-at-point wgrep unfill smex smeargle orgit mwim mmm-mode markdown-toc markdown-mode magit-gitflow ivy-hydra gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md fuzzy flyspell-correct-ivy flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit with-editor diff-hl counsel-projectile counsel swiper ivy company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async))))
